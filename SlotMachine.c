50aa1fcbe8877db28fe3a9ee777c6957a2aa2171
Jadey <jade.yamoah@googlemail.com>
2021-02-10 12:22:41 +0000

    (Init)ialisation

---
+++ SlotMachine.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void spin()
+{
+    //Assigns a symbol to each reel
+}
+
+int result()
+{
+    //Returns the result of the spin
+}
+
+int reward()
+{
+    //Calculates the player's reward depending on the result
+}
+
+int main()
+{
+    
+}
