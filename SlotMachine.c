e25dd46db305d04a65c667dee78b28afa47b9c89
Jade <jade.yamoah@googlemail.com>
2021-04-24 20:16:14 +0100

    The spin function now assigns text(symbols) to each reel

--- SlotMachine.c
+++ SlotMachine.c
@@ -12,16 +12,39 @@ Wishlist:
 -User can input the number of reels
 */
 
-void spin()
+void spin() //assigns reels "symbols"
 {
     srand(time(NULL));
 
     int random = rand() % 12 + 1;
     //printf("The randomly generated value is %d.\n", random);
     char reels[3][10];  //creating a character array of strings with a max length of 10
-    int spins = 3;
-    int numberOfReels = 0; //This is for when the user input comes into play
 
+    //This is for when the user input comes into play
+    //When this works, user will input number between a range (number of reels)
+    //Then the spins variable will be set to this value
+    int numberOfReels = 3; 
+
+    int spins = numberOfReels;
+
+    for(int i = 0; i < spins; i++)
+    {
+        int random = rand()%12 + 1;
+        if(random == 0 || random == 1 || random == 2 )
+        {
+            strcpy(reels[i], "bell");
+
+        } else if(random == 3 || random == 4 || random == 5) {
+            strcpy(reels[i], "orange");
+            
+        } else if(random == 6 || random == 7 || random == 8){
+            strcpy(reels[i], "cherry");
+
+        } else {
+            strcpy(reels[i], "horseshoe");
+
+        }
+    }
 }
 
 //check function that compares symbols on the reels to each other (no matter how many reels)
